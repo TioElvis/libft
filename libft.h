9c030e1eb5c47a43dbd1309434663fd864fc06d4
TioElvis <elvis.veramijares@gmail.com>
2024-12-22 18:18:27 +0100

    strncmp

--- libft.h
+++ libft.h
@@ -14,5 +14,6 @@ int ft_toupper(int c);
 int ft_tolower(int c);
 char *ft_strchr(const char *str, int c);
 char *ft_strrchr(const char *str, int c);
+int ft_strncmp(const char *str1, const char *str2, size_t n);
 
 #endif
